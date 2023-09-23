f42792bc877ebf3bd06556043616985e45184b14
Paul Shults <106405120+shulzpavel@users.noreply.github.com>
2023-09-23 18:57:36 +0300

    feat: добавил проект

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const contactsData = [
+  {
+    "имя": "Иван",
+    "фамилия": "Иванов",
+    "телефон": "+7 123-456-7890"
+  },
+  {
+    "имя": "Екатерина",
+    "фамилия": "Смирнова",
+    "телефон": "+7 987-654-3210"
+  },
+  {
+    "имя": "Алексей",
+    "фамилия": "Петров",
+    "телефон": "+7 555-555-5555"
+  }
+];
+
+function ContactList({ contacts }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+      {contacts.map((contact, index) => (
+        <li key={index}>
+          <strong>{contact.имя} {contact.фамилия}</strong>: {contact.телефон}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortedContacts, setSortedContacts] = useState(contactsData);
+
+  useEffect(() => {
+    // Функция для фильтрации контактов по имени
+    const filteredContacts = contactsData.filter(contact =>
+      contact.имя.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    // Сортировка контактов по имени
+    const sortedFilteredContacts = filteredContacts.sort((a, b) =>
+      a.имя.localeCompare(b.имя)
+    );
+
+    setSortedContacts(sortedFilteredContacts);
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <h1>Книжка контактов</h1>
+      <input
+        type="text"
+        placeholder="Поиск по имени"
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+      />
+      <ContactList contacts={sortedContacts} />
     </div>
   );
 }
 
 export default App;
+
